c9599f4bc5abe1fac8149c95cead586726f6391c
Emiliojoa <emiliojoaquinortizmalich@gmail.com>
2025-12-13 22:56:00 -0300

    feat: :art: se arreglo es estilo que fallaba en celulares

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 export const Hero = () => {
   return (
-    <section className="relative overflow-hidden py-28 mx-44 rounded-xl md:px-8">
+    <section className="relative overflow-hidden   rounded-xl md:px-8">
       <div className="mx-auto text-center relative  w-3/4 p-4 rounded">
         <div className="py-4 flex flex-col items-center justify-center">
-            <img src="./banner.png" alt="banner" className="" />
+          <img src="./banner.png" alt="banner" className="" />
           <h3 className="text-3xl text-gray-800 font-semibold md:text-4xl">
             GotyBot - Tu asistente inteligente para documentos
           </h3>
